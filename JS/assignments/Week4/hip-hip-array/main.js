2bbfebe93d29faf85763ed14a536c4f36f8aafab
Ceay44 <cwilliams@perseverenow.org>
2024-06-05 15:06:00 -0500

    test

--- JS/assignments/Week4/hip-hip-array/main.js
+++ JS/assignments/Week4/hip-hip-array/main.js
@@ -22,13 +22,17 @@ const students = [
   'Arnold',
   'Andy'
 ];
-
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i]);
 
 // Challenge 2
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
-
-
+for (let i = grades.length - 1; i >= 0; i--) {
+  console.log(grades[i])
+ 
+}
+console.log(grades[i])
 // Challenge 3
 // Console.log out only the even numbers in the following array.
 const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
