6fb593cb38fc7893aa7b5b2527178736aaf8fd98
Ceay44 <cwilliams@perseverenow.org>
2024-06-06 06:25:53 -0500

    test

--- JS/assignments/Week4/hip-hip-array/main.js
+++ JS/assignments/Week4/hip-hip-array/main.js
@@ -30,31 +30,60 @@ for (let i = 0; i < students.length; i++) {
 const grades = [100, 80, 110, 75, 83, 64];
 for (let i = grades.length - 1; i >= 0; i--) {
   console.log(grades[i])
- 
 }
 console.log(grades[i])
+
 // Challenge 3
 // Console.log out only the even numbers in the following array.
 const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
-
+for (let i = 0; i < positiveNumbers.length; i++) {
+  if (positiveNumbers[i] % 2 === 0) {
+    console.log(positiveNumbers[i]);
+  }
+}
 
 // Challenge 4
 // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
 const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
-
+for (let i = 0; i < mixedSignNumbers.length; i++) {
+  if (mixedSignNumbers[i] % 2 === 0) {
+    console.log(mixedSignNumbers[i]);
+  }
+}
 
 // Challenge 5
 // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
 const symmetricalCapitals = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y'];
+const removedItemsFromBeginning = symmetricalCapitals.splice(0, 2); // Remove two values from the beginning
+const removedItemFromEnd = symmetricalCapitals.pop(); // Remove one value from the end
 
+removedItemsFromBeginning.forEach(value => {
+  console.log(value);
+});
+
+symmetricalCapitals.forEach(value => {
+  console.log(value);
+});
+
+console.log(removedItemFromEnd);
 
 // Challenge 6
 // Add a value (your choice!) to the start of the following array and two values (of your choice) to the end simply by adding them in the following line, and then console.log out each value individually.
 const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 
+fibonacciNumbers.unshift(0); // Add a value to the start
+fibonacciNumbers.push(21, 34); // Add two values to the end
+
+fibonacciNumbers.forEach(value => {
+  console.log(value);
+});
 
 // Challenge 7
 // Make a NEW array with 5 values of your choice, using the same syntax as the array creation from the previous 6 challenges; that is, every value should be placed within the square brackets.  Now console.log out each value individually.
+const myArr = ["t","o","d","a","y"];
+myArr.forEach(value => {
+  console.log(value);
+});
 
 
 // Challenge 8
